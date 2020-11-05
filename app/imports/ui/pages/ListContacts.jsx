e16c14247b8381ff6fd076a7dd14d1782441e723
Alvin Yang <alvinyang101@gmail.com>
2020-11-04 23:24:25 -1000

    DNF

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -4,31 +4,11 @@ import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contact/Contacts';
 import Contact from '../components/Contact';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListContacts extends React.Component {
-  contacts = [{
-    firstName: 'Philip', lastName: 'Johnson', address: 'POST 307, University of Hawaii',
-    image: 'https://philipmjohnson.github.io/images/philip2.jpeg',
-    description: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-        'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-  },
-    {
-      firstName: 'Henri', lastName: 'Casanova', address: 'POST 307, University of Hawaii',
-      image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
-      description: 'I am originally from France. I maintain a list of reports from my surf sessions. I have proof ' +
-          'that I ran the Hana relay with an actual Team.',
-    },
-    {
-      firstName: 'Kim', lastName: 'Binsted', address: 'POST 307, University of Hawaii',
-      image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-      description: 'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence' +
-          'from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of ' +
-          'punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as ' +
-          '"What do you call a Martian who drinks beer? An ale-ien!".',
-    },
-  ];
 
 /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -52,6 +32,7 @@ class ListContacts extends React.Component {
 
 ListContacts.propTypes = {
   stuffs: PropTypes.array.isRequired,
+  contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -61,6 +42,7 @@ export default withTracker(() => {
   const subscription = Meteor.subscribe(Stuffs.userPublicationName);
   return {
     stuffs: Stuffs.collection.find({}).fetch(),
+    contacts: Contacts.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
 })(ListContacts);
